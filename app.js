e7791aa1ee1d90ef68202775684da52cf3ab55b0
SophieMaryam <sophievanwersch@hotmail.com>
2017-08-30 11:33:31 +0200

    changed sprite size, bullets working, added tiles

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views',__dirname + '/src/views');
+app.set('view engine', 'pug');
 
 // Game 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.render('index');
 });
 
 app.listen(8080, () => {
-	console.log("Listening")
+	console.log("Listening");
+	console.log(__dirname);
 });
 
 
@@ -20,192 +24,190 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 // const bcrypt = require('bcrypt');
 
-// const Sequelize = require('sequelize');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const sequelize = new Sequelize('final_project', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-// 	host: 'localhost',
-// 	dialect: 'postgres',
-// 	storage: './session.postgres',
-// 	define: {
-// 		timestamps: false
-// 	}
-// });
+const Sequelize = require('sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = new Sequelize('final_project', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+	host: 'localhost',
+	dialect: 'postgres',
+	storage: './session.postgres',
+	define: {
+		timestamps: false
+	}
+});
 
 // Setting up Pug
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'pug');
 
 
-// app.use('/', bodyParser.urlencoded({extended:true}));
-// // app.use(session({
-// 	secret: "This is a secret",
-// 	resave:false,
-// 	saveUninitialized: true,
-// 	// store: new SequelizeStore({
-// 	// 	db: sequelize,
-// 	// 	checkExpirationInterval: 30 * 60 * 1000,
-// 	// 	expiration : 24 * 60 * 60 * 1000
-// 	// })
-// }));
 
-// // Model Configuration
+app.use('/', bodyParser.urlencoded({extended:true}));
+app.use(session({
+	secret: "This is a secret",
+	resave:false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+		checkExpirationInterval: 30 * 60 * 1000,
+		expiration : 24 * 60 * 60 * 1000
+	})
+}));
 
-// const User = sequelize.define('user',{
-// 	firstname: {
-//         type: Sequelize.STRING
-//     },
-//     lastname: {
-//         type: Sequelize.STRING
-//     },
-//     username: {
-//         type: Sequelize.STRING,
-//         unique:true
-//     },
-//     email: {
-//         type: Sequelize.STRING,
-//         unique:true,
-//     },
-//     password: {
-//         type: Sequelize.STRING,
-//     },
-//     password_confirmation: {
-//         type: Sequelize.STRING,
-//         validate: {
-//             notEmpty:true
-//         }
-//     }
-//     }); 
+// Model Configuration
 
-// sequelize.sync({force:false});
+const User = sequelize.define('user',{
+	firstname: {
+        type: Sequelize.STRING
+    },
+    lastname: {
+        type: Sequelize.STRING
+    },
+    username: {
+        type: Sequelize.STRING,
+        unique:true
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique:true,
+    },
+    password: {
+        type: Sequelize.STRING,
+    },
+    password_confirmation: {
+        type: Sequelize.STRING,
+        validate: {
+            notEmpty:true
+        }
+    }
+    }); 
 
-// // Home page
-// app.get('/',(req,res) => {
-// 	var user = req.session.user;
-// 	res.render('index', {user:user})
-// });
+sequelize.sync({force:false});
 
-// // Register
+// Home page
+app.get('/',(req,res) => {
+	var user = req.session.user;
+	res.render('index', {user:user})
+});
 
-// // Register
-// app.get('/register',(req,res) => {
-// 	var user = req.session.user;
-// 	res.render('register', {user:user})
-// });
 
-// app.post('/register', (req,res) => {
-// 	let password = req.body.inputPassword,
-// 	email = req.body.inputEmail,
-// 	firstname = req.body.inputFirstName,
-// 	lastname = req.body.inputLastName,
-// 	pwconfirmation = req.body.passwordconfirmation;
-// 	console.log('pwconfirmation ' + pwconfirmation);
-// 	console.log('password: ' + password);
+// Register
+app.get('/register',(req,res) => {
+	var user = req.session.user;
+	res.render('register', {user:user})
+});
+
+app.post('/register', (req,res) => {
+	let password = req.body.inputPassword,
+	email = req.body.inputEmail,
+	firstname = req.body.inputFirstName,
+	lastname = req.body.inputLastName,
+	pwconfirmation = req.body.passwordconfirmation;
+	console.log('pwconfirmation ' + pwconfirmation);
+	console.log('password: ' + password);
 	
-// 	if(password !== pwconfirmation){
-// 		throw new Error("Password confirmation doesn't match.")
-// 	} else if (password === pwconfirmation) {
-// 		bcrypt.hash(password, 10, (err, hash) => {
-// 			console.log("the hash" + hash)
-// 			if(err){
-// 				throw err;
-// 			}
+	if(password !== pwconfirmation){
+		throw new Error("Password confirmation doesn't match.")
+	} else if (password === pwconfirmation) {
+		bcrypt.hash(password, 10, (err, hash) => {
+			console.log("the hash" + hash)
+			if(err){
+				throw err;
+			}
 
-// 		User.create({
-// 			firstname:firstname,
-// 			lastname:lastname,
-// 			email: email,
-// 			password: hash,
-// 			password_confirmation: hash
-// 		})
-// 		.then((user) => {
-// 			req.session.user = user;
-// 			res.redirect(`/profile`);
-// 		})
-// 		.catch((err) => {
-// 			console.log("Error" + err);
-// 		})
+		User.create({
+			firstname:firstname,
+			lastname:lastname,
+			email: email,
+			password: hash,
+			password_confirmation: hash
+		})
+		.then((user) => {
+			req.session.user = user;
+			res.redirect(`/profile`);
+		})
+		.catch((err) => {
+			console.log("Error" + err);
+		})
 	
-// 	})
-// 	}
-// });
+	})
+	}
+});
 
  
-// // Login 
-// app.get('/login', (req,res) => {
-// 	var user = req.session.user;
-// 	if(!user){
-// 		res.render('login')
-// 	} else {
-// 		res.redirect(`/profile`)
-// 	}
-// });
+// Login 
+app.get('/login', (req,res) => {
+	var user = req.session.user;
+	if(!user){
+		res.render('login')
+	} else {
+		res.redirect(`/profile`)
+	}
+});
 
 
-// app.post('/login', bodyParser.urlencoded({extended:true}),(req,res) => {
+app.post('/login', bodyParser.urlencoded({extended:true}),(req,res) => {
+	User.findOne({
+	where: {
+	  email: req.body.inputEmail
+	}
+	})
+	.then ((user) => {
+	console.log('does it reach this??' + user)
+	   if(!user){
+				res.redirect('/?message=' + encodeURIComponent("Invalid email or password"));
+		} else {
+			console.log('nnumber 2')
+			bcrypt.compare(req.body.inputPassword, user.password, (err, result) => { // first argument is the password the user typed in, and thes second is the one in the database
+				if(err){
+					console.log(err)
+				} else {
+					if(result === true){
+						req.session.user = user;
+						res.redirect(`/profile`)
+					} else {
+						console.log("Error")
+						res.redirect('/?message=' + encodeURIComponent("Invalid password"));
+					}
+				}
+			})
+		}
+	})
+	.catch(function(err){
+		console.log("Error" + err)
+		res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+	});
+}); 
 
-// 	User.findOne({
-// 	where: {
-// 	  email: req.body.inputEmail
-// 	}
-// 	})
-// 	.then ((user) => {
-// 	console.log('does it reach this??' + user)
-// 	   if(!user){
-// 				res.redirect('/?message=' + encodeURIComponent("Invalid email or password"));
-// 		} else {
-// 			console.log('nnumber 2')
-// 			bcrypt.compare(req.body.inputPassword, user.password, (err, result) => { // first argument is the password the user typed in, and thes second is the one in the database
-// 				if(err){
-// 					console.log(err)
-// 				} else {
-// 					if(result === true){
-// 						req.session.user = user;
-// 						res.redirect(`/profile`)
-// 					} else {
-// 						console.log("Error")
-// 					}
-// 				}
-// 			})
-// 		}
-// 	})
-// 	.catch(function(err){
-// 		console.log("Error" + err)
-// 		res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
-// 	});
-// }); 
+	// log out
 
-// 	// log out
+app.get('/logout',(req,res) => {
+  req.session.destroy((err) => {
+    if (err){
+      throw (err);
+    }
+    res.redirect('/?login=' + encodeURIComponent('Logged out successfully'));
+  });
+});
 
-// app.get('/logout',(req,res) => {
-//   req.session.destroy((err) => {
-//     if (err){
-//       throw (err);
-//     }
-//     res.redirect('/?login=' + encodeURIComponent('Logged out successfully'));
-//   });
-// });
+// Profile 
 
-// // Profile 
+app.get('/profile',(req,res) => {
+  var user = req.session.user;
 
-// app.get('/profile',(req,res) => {
-//   var user = req.session.user;
+  if(!user){
+		res.redirect('/?message=' + encodeURIComponent("Please log in."));
+	} else {
+		res.render('profile', {user: user});
+	}
+});
 
-//   if(!user){
-// 		res.redirect('/?message=' + encodeURIComponent("Please log in."));
-// 	} else {
-// 		res.render('profile', {user: user});
-// 	}
-// });
+// Chat
 
-// // Chat
-
-// app.get('/chat', (req, res) => {
-// 	var user = req.session.user
-// 	 if(!user){
-// 		res.redirect('/?message=' + encodeURIComponent("Please log in."));
-// 	} else {
-// 		res.render('profile', {user: user});
-// 	}
-// });
+app.get('/chat', (req, res) => {
+	var user = req.session.user
+	 if(!user){
+		res.redirect('/?message=' + encodeURIComponent("Please log in."));
+	} else {
+		res.render('profile', {user: user});
+	}
+});
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,13 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 		game.load.image('background', 'assets/images/background.png'); 
 		game.load.image('star', 'assets/images/star.png');
 		game.load.atlasJSONHash('bot', 'assets/sprites/s.png', 'assets/sprites/sprites.json');	
+		game.load.image('bullet', 'assets/images/bullet.png');
 		game.load.image('ground', 'assets/images/platform.png');
 		game.load.image('tile', 'assets/images/tile.png');
+		game.load.image('tilepiece', 'assets/images/tile_00.png');
+		game.load.image('tilepieces', 'assets/images/tile_02.png');
 		game.load.image('tileground', 'assets/images/tileground.png');	
-		game.load.image('bullets', 'assets/images/bullet.png');
+		game.load.image('enemy', 'assets/images/enemy.png');
 	}
 
 	var background;
@@ -17,7 +20,6 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 	var jumpButton;
  	var platforms;
  	var ground;
- 	var ledge;
  	var tile;
  	var cursors;
  	var score = 0;
@@ -26,6 +28,9 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
  	var fireButton;
  	var fireRate = 200;
     var nextFire = 0; 
+    var enemies;
+    var jumpButton;
+    var jumpTimer;
 
 
 	function create(){
@@ -44,9 +49,18 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 		
 		tile = platforms.create(400, 400, 'tile');
 		tile.body.immovable = true;
-		tile = platforms.create(-150, 250, 'tile');
+		
+		tile = platforms.create(200, 300, 'tilepiece');
 		tile.body.immovable = true;
-		tile = platforms.create(400, 50, 'tile');
+		
+		tile = platforms.create(50, 50, 'tile');
+		tile.body.immovable = true;
+
+
+		tile = platforms.create(100, 200, 'tilepiece');
+		tile.body.immovable = true;
+
+		tile = platforms.create(300, 100, 'tilepieces');
 		tile.body.immovable = true;
 		
 	
@@ -55,7 +69,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 	  
 
 	    player.anchor.setTo(0.5, 0.5);
-	    player.scale.setTo(2, 2);
+	    player.scale.setTo(1, 1);
 	    game.physics.enable(player, Phaser.Physics.ARCADE);
 
 	    player.body.bounce.y = 0.2;
@@ -67,7 +81,22 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 
 		game.camera.follow(player);
 
-		
+		jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+		// bad guys
+		enemies = game.add.group();
+	    enemies.enableBody = true;
+	   	enemies.physicsBodyType = Phaser.Physics.ARCADE;
+	    enemies.createMultiple(5, 'enemy');
+	  	enemies.setAll('anchor.x', 0.5);
+	    enemies.setAll('anchor.y', 0.5);
+	    enemies.setAll('scale.x', 0.5);
+	    enemies.setAll('scale.y', 0.5);
+	    enemies.setAll('angle', 180);
+	    enemies.setAll('outOfBoundsKill', true);
+	    enemies.setAll('checkWorldBounds', true);
+
+		// stars
 		stars = game.add.group();
 		stars.enableBody = true;
 
@@ -76,25 +105,21 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 		        star.body.gravity.y = 300;
 		        star.body.bounce.y = 0.7 + Math.random() * 0.2;
 		}
+		//score 
 		scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 
-		bullets = game.add.group;
+		//bullets
+		bullets = game.add.group();
 		bullets.enableBody = true;
 		bullets.physicsBodyType = Phaser.Physics.ARCADE;
-		bullets.createMultiple(50, 'bullets');
-		// bullets.setAll('anchor.x', 0.5);
-		// bullets.setAll('anchor.y', 1);
+		bullets.createMultiple(50, 'bullet');
+		bullets.setAll('anchor.x', 0.5);
+		bullets.setAll('anchor.y', 1);
 		bullets.setAll('outOfBoundsKill', true);
 		bullets.setAll('checkWorldBounds', true);
-		bullets.setAll('body.gravity.y', 0);
 		fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
-		
-		if (fireButton.isDown || game.input.activePointer.isDown)
-	   			{
-	        		fireBullet();
-	        		player.animations.play('')
-	    		}
 	}
+
 
 	function update() {
 
@@ -102,6 +127,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 		game.physics.arcade.collide(stars, platforms);
 		game.physics.arcade.overlap(player, stars, collectStar, null, this);
 		player.body.velocity.x = 0;
+
 
 	    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
 		    {
@@ -120,15 +146,22 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 
 			}
 
-	    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
-		    {
-		        player.body.velocity.y = -200;
-		    }
-	    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-		    {
-		       player.y += 4;
-		   	}
-		
+	if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+	    {
+	       player.y += 4;	
+	    }
+	if (fireButton.isDown || game.input.activePointer.isDown) 
+		{
+      	  fireBullet();
+    	}
+  	
+  	if (jumpButton.isDown && player.body.touching.down && game.time.now > jumpTimer)
+    	{
+	        player.body.velocity.y = -200;
+	        jumpTimer = game.time.now + 750
+        	// s.frame = 9;
+    	}
+	
 	}
 
 	function render(){
@@ -136,7 +169,6 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 	}
 
 	function collectStar (player, star) {
-
 	    star.kill();
 
 	    score += 10;
@@ -144,25 +176,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_game', { preload: prel
 	}
 
 	function fireBullet() {
+		  
+		  var bullet = bullets.getFirstExists(false);
 
-	    if (game.time.now > nextFire && bullets.countDead() > 0)
-	    {
-	    	var bullet = bullets.getFirstExists(false);
-				if (bullet)
-					{
-
-						bullet.reset(player.x, player.y + 8);
-						bullet.body.velocity.y = -400;
-					}    
-
-		        // nextFire = game.time.now + fireRate;
-
-	        // bullets = bullets.getFirstDead();
-
-	        // bullets.reset(player.x+5, player.y -45);
-
-	        // bullets.body.velocity.x = 500;
-
-	        // game.physics.arcade.moveToPointer(bullets, 400);
-	    }
+    if (bullet)
+    {
+        //  And fire it
+        bullet.reset(player.x+5, player.y-45);
+        // nextFire = game.time.now + fireRate;
+        bullet.body.velocity.x = 500;
+        game.physics.arcade.moveToPointer(bullet, 400);
+    }
+    	
 	}
